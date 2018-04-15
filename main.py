9763d423845f4d9ef0e6258a7d3febdf64f51609
Thierno Sadou Diallo <thierved01@gmail.com>
2018-04-15 14:06:17 +0000

    still learning

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from airplan import Airplan
+from car import Car
+from boat import Boat
 
 def main():
-    print('wellcome!')
+    
+    vehicules = [Car(50), Airplan(5000), Boat(210)]
 
-
-
-
-
-
+    for v in vehicules:
+            v.move()
 
 if __name__ == '__main__': main()
 
